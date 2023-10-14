5034bc69282dcd8ea265d3c2dda0d08dea52be63
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-14 22:35:07 +0300

    feat: create the /signup page

---
+++ front/src/app/signup/handleSignup.ts
@@ -0,0 +1,33 @@
+"use server";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export async function handleSignup(
+  login: string,
+  password: string,
+): Promise<string> {
+  const response = await fetch(`${process.env.SERVER_URL}/auth/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ login, password }),
+  });
+
+  if (response.ok) {
+    const cookieHeader = response.headers.getSetCookie()[0];
+    const cleanHeader = cookieHeader.replace("session=", "");
+    const semicolonIndex = cleanHeader.indexOf(";");
+    const sessionToken = cleanHeader.substring(0, semicolonIndex);
+
+    cookies().set("session", sessionToken, {
+      path: "/",
+      httpOnly: true,
+      maxAge: 86400 * 30,
+    });
+
+    redirect("/chat");
+  }
+
+  return response.text();
+}
--- front/src/app/signup/page.tsx
+++ front/src/app/signup/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { SignupForm } from "./signupForm";
 
 export default function Signup() {
   return (
     <main>
       <h2>Signup page</h2>
-      {/* SignupForm */}
+      <SignupForm />
       <div>
         Already have an accound?{" "}
         <Link href="/login" className="text-purple-500">
