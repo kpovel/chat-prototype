873ac9d82ed9caa2cb299c2b235fed6010e1519a
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-17 18:45:22 +0300

    feat: make the home page available in two languages

---
+++ front/src/app/[lang]/page.tsx
@@ -0,0 +1,35 @@
+import { promises as fs } from "fs";
+import Link from "next/link";
+
+// todo: multi language stuff
+// should I do this in layout.tsx?
+
+export async function generateStaticParams() {
+  return [{ lang: "en" }, { lang: "uk" }];
+}
+
+async function pageContent(lang: string) {
+  const fileContent = await fs.readFile(
+    `${process.cwd()}/src/app/[lang]/${lang}.json`,
+    "utf8",
+  );
+  return JSON.parse(fileContent);
+}
+
+export default async function Home({ params }: { params: { lang: string } }) {
+  const content = await pageContent(params.lang);
+
+  return (
+    <main>
+      <h2>{content.pageHeader}</h2>
+      <div className="flex gap-4">
+        <Link href={`/${params.lang}/login`} className="p-1 px-3 border rounded-xl bg-gray-400">
+          {content.login}
+        </Link>
+        <Link href={`/${params.lang}/signup`} className="p-1 px-3 border rounded-xl bg-gray-400">
+          {content.signup}
+        </Link>
+      </div>
+    </main>
+  );
+}
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <h2>Chat prototype</h2>
-      <div className="flex gap-4">
-        <Link href="/login" className="p-1 px-3 border rounded-xl bg-gray-400">
-          Login
-        </Link>
-        <Link href="/signup" className="p-1 px-3 border rounded-xl bg-gray-400">
-          Signup
-        </Link>
-      </div>
-    </main>
-  );
+  redirect("/en")
 }
