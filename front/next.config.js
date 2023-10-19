b9c6ae449fa9c7935c70a4f2a504b9257ec6119d
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-19 10:29:27 +0300

    this should work

--- front/next.config.js
+++ front/next.config.js
@@ -3,18 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.json$/,
-        loader: 'json-loader',
-        type: 'javascript/auto',
-        include: /app/,
-      });
-    }
-
-    return config;
-  },
 }
 
 module.exports = nextConfig
