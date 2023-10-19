78cf1040c37f637e70c9dc92fe9792c41b74b273
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-19 10:24:29 +0300

    would it fix reading json files?

--- front/next.config.js
+++ front/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.json$/,
+        loader: 'json-loader',
+        type: 'javascript/auto',
+        include: /app/,
+      });
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
