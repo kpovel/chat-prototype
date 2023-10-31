fc30462ba905e1eaef15f87bd07197130aedad5a
Pavlo Karas <kar.pavel123@gmail.com>
2023-10-31 13:51:55 +0200

    fix: allow WebSocket connection for any origin

--- back/chat/chat.go
+++ back/chat/chat.go
@@ -8,32 +8,33 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-  ReadBufferSize: 1024,
-  WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 func Chat(w http.ResponseWriter, r *http.Request) {
-  conn, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-    log.Println(w, err)
-    return
-  }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(w, err)
+		return
+	}
 
-  defer conn.Close()
+	defer conn.Close()
 
-  for {
-    messageType, message, err := conn.ReadMessage()
-    if err != nil {
-      log.Println(w, err)
-      return
-    }
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(w, err)
+			return
+		}
 
-    log.Printf("Received: %s\n", message)
+		log.Printf("Received: %s\n", message)
 
-    err = conn.WriteMessage(messageType, message)
-    if err != nil {
-      log.Println(w, err)
-      return
-    }
-  }
+		err = conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println(w, err)
+			return
+		}
+	}
 }
